adb7c414869e72d16f2d09711f920a68b3469576
Mohamed <MansourMohamedAli1996@gmail.com>
2024-10-03 22:20:59 -0400

    Commands can be seperated by ;.

--- RemoteCMDServer.py
+++ RemoteCMDServer.py
@@ -4,6 +4,9 @@ import os
 import sys
 from logger import logger
 
+# TODO handle backslash character
+
+
 # Define the server address and port
 SERVER_HOST_NAME = socket.gethostname()
 IP_ADDRESS = socket.gethostbyname(SERVER_HOST_NAME)
@@ -31,30 +34,35 @@ def main():
         while True:
             client_socket, client_address = server_socket.accept()
             logger.debug(f"Accepted connection from {client_address}")
-            command = client_socket.recv(1024).decode()
-            if command:
-                if command.lower() == 'exit':
-                    logger.info("Exiting...")
-                    sys.exit()
-
-                # Check if the command is a 'cd' command
-                if command.startswith('cd '):
-                    try:
-                        new_dir = command[3:].strip()
+            full_command = client_socket.recv(1024).decode()
+            if ';' in full_command:
+                full_command = full_command.split(';')
+            else:
+                full_command = [full_command]
+            for command in full_command:
+                if command:
+                    if command.lower() == 'exit':
+                        logger.info("Exiting...")
+                        sys.exit()
+                    # Check if the command is a 'cd' command
+                    if command.startswith('cd '):
+                        try:
+                            new_dir = command[3:].strip()
+                            os.chdir(new_dir)
+                            cwd = os.getcwd()  # Update the current working directory
+                            logger.info(command)
+                        except FileNotFoundError as e:
+                            logger.error(f"Error: {e}")
+                    elif len(command) > 1 and command[1] == ':': # Changing Drive
+                        new_dir = command[:2].strip()
                         os.chdir(new_dir)
                         cwd = os.getcwd()  # Update the current working directory
                         logger.info(command)
-                    except FileNotFoundError as e:
-                        logger.error(f"Error: {e}")
-                elif len(command) > 1 and command[1] == ':': # Changing Drive
-                    new_dir = command[:2].strip()
-                    os.chdir(new_dir)
-                    cwd = os.getcwd()  # Update the current working directory
-                    logger.info(command)
-                else:
-                    # Execute the command and get the output
-                    logger.info(f'Executing {command}')
-                    execute_command(command, cwd)
+                    else:
+                        # Execute the command and get the output
+                        logger.info(f'Executing {command}')
+                        print('=' *25 + ' Output ' + '=' *25)
+                        execute_command(command, cwd)
 
     except socket.error as e:
         logger.error(f"Socket error: {e}")
