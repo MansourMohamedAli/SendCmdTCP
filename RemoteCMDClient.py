0d5bde94dce97b0702e2b2300d9986c7d5006818
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2024-11-21 21:31:24 -0500

    changed name of argument to iplocal

--- RemoteCMDClient.py
+++ RemoteCMDClient.py
@@ -8,13 +8,13 @@ DEFAULT_SERVER_PORT = 52000
 DEFAULT_MAX_ATTEMPTS = 1  # Maximum number of connection attempts
 
 
-def connect_to_server(server_host_ip, server_host_port, max_retries, interface):
+def connect_to_server(server_host_ip, server_host_port, max_retries, iplocal):
     """Attempt to connect to the server and return the socket object."""
     attempts = 0 
     while attempts < max_retries:
         try:
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.bind((interface, 0))
+            client_socket.bind((iplocal, 0))
             client_socket.connect((server_host_ip, server_host_port))
             logger.info("Connected to server")
             return client_socket
@@ -25,14 +25,12 @@ def connect_to_server(server_host_ip, server_host_port, max_retries, interface):
         logger.critical(f'Could not connect to server after {max_retries} attempt(s)... Exiting')
 
 
-def open_connection_thread(server_host_ip, server_host_port, command, max_attempts, interface):
+def open_connection_thread(server_host_ip, server_host_port, command, max_attempts, iplocal):
     """New thread is created to not block code execution of GUIs that use this application."""
-    client_socket = connect_to_server(server_host_ip, server_host_port, max_attempts, interface)
+    client_socket = connect_to_server(server_host_ip, server_host_port, max_attempts, iplocal)
     if client_socket:
         while True:
             try:
-                # t1 = threading.Thread(target=client_socket.send, args=[command.encode()])
-                # t1.start()
                 client_socket.send(command.encode())
                 logger.info(f"Command '{command}' sent to the server.")
                 break
@@ -57,7 +55,7 @@ def main(args=None):
         args = sys.argv[1:]
         
         if len(sys.argv) < 2:
-            logger.info(f"Type 'RemoteCMDClient.exe -h' or 'RemoteCMDClient.exe --help' for usage.")
+            print(f"Type 'RemoteCMDClient.exe -h' or 'RemoteCMDClient.exe --help' for usage.")
             return
 
     parser = argparse.ArgumentParser(description='Client for sending commands to the server.')
@@ -65,18 +63,20 @@ def main(args=None):
     parser.add_argument("command", type=str, help='Command to send. If command is multiple words, enclose in \"\".')
     parser.add_argument('--port', type=int, default=DEFAULT_SERVER_PORT, help=f'The port to connect to the server. Default is {DEFAULT_SERVER_PORT}.')
     parser.add_argument('--attempts', type=int, default=DEFAULT_MAX_ATTEMPTS, help=f'The maximum number of connection attempts. Default is {DEFAULT_MAX_ATTEMPTS}.')
-    parser.add_argument('--interface', type=str, default='', help=f'IP address of local network interface used to send command.')
+    parser.add_argument('--iplocal', type=str, default='', help=f'IP address of local network interface used to send command. Default will bind to all interfaces.')
     args = parser.parse_args(args)
 
     server_host_ip   = args.host 
     command          = args.command
     server_host_port = args.port
     max_attempts     = args.attempts
-    interface        = args.interface
+    iplocal          = args.iplocal
 
+    # Get IP Address if a name is passed in by user.
     server_host_ip = socket.gethostbyname(server_host_ip)
 
-    connection_thread = threading.Thread(target=open_connection_thread, args=[server_host_ip, server_host_port, command, max_attempts, interface])
+    # Start connection in a thread.
+    connection_thread = threading.Thread(target=open_connection_thread, args=[server_host_ip, server_host_port, command, max_attempts, iplocal])
     connection_thread.start()
     
 
