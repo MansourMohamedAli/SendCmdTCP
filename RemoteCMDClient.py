94559d57cb9057a437681d1a7e72cea099ea61db
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2024-11-21 21:17:17 -0500

    Added Interface option so user can specify local network IP in which to send commands from

--- RemoteCMDClient.py
+++ RemoteCMDClient.py
@@ -8,12 +8,13 @@ DEFAULT_SERVER_PORT = 52000
 DEFAULT_MAX_ATTEMPTS = 1  # Maximum number of connection attempts
 
 
-def connect_to_server(server_host_ip, server_host_port, max_retries):
+def connect_to_server(server_host_ip, server_host_port, max_retries, interface):
     """Attempt to connect to the server and return the socket object."""
     attempts = 0 
     while attempts < max_retries:
         try:
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.bind((interface, 0))
             client_socket.connect((server_host_ip, server_host_port))
             logger.info("Connected to server")
             return client_socket
@@ -24,9 +25,9 @@ def connect_to_server(server_host_ip, server_host_port, max_retries):
         logger.critical(f'Could not connect to server after {max_retries} attempt(s)... Exiting')
 
 
-def open_connection_thread(server_host_ip, server_host_port, command, max_attempts):
+def open_connection_thread(server_host_ip, server_host_port, command, max_attempts, interface):
     """New thread is created to not block code execution of GUIs that use this application."""
-    client_socket = connect_to_server(server_host_ip, server_host_port, max_attempts)
+    client_socket = connect_to_server(server_host_ip, server_host_port, max_attempts, interface)
     if client_socket:
         while True:
             try:
@@ -64,16 +65,18 @@ def main(args=None):
     parser.add_argument("command", type=str, help='Command to send. If command is multiple words, enclose in \"\".')
     parser.add_argument('--port', type=int, default=DEFAULT_SERVER_PORT, help=f'The port to connect to the server. Default is {DEFAULT_SERVER_PORT}.')
     parser.add_argument('--attempts', type=int, default=DEFAULT_MAX_ATTEMPTS, help=f'The maximum number of connection attempts. Default is {DEFAULT_MAX_ATTEMPTS}.')
+    parser.add_argument('--interface', type=str, default='', help=f'IP address of local network interface used to send command.')
     args = parser.parse_args(args)
 
     server_host_ip   = args.host 
     command          = args.command
     server_host_port = args.port
     max_attempts     = args.attempts
+    interface        = args.interface
 
     server_host_ip = socket.gethostbyname(server_host_ip)
 
-    connection_thread = threading.Thread(target=open_connection_thread, args=[server_host_ip, server_host_port, command, max_attempts])
+    connection_thread = threading.Thread(target=open_connection_thread, args=[server_host_ip, server_host_port, command, max_attempts, interface])
     connection_thread.start()
     
 
