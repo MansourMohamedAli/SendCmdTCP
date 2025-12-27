3969e78a0feb70462e1d11bec7141aba4147a559
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2025-12-27 09:58:30 -0500

    Can send commands from json file config or through command line use.

--- SendCmdClient.py
+++ SendCmdClient.py
@@ -24,31 +24,53 @@ async def tcp_echo_client(host, port, message):
     await writer.wait_closed()
     return f"Send Command {message}"
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run commands on one or more hosts"
+    )
+
+    # Optional positionals
+    parser.add_argument("hostname", nargs="?", type=str, help="Server IP address.")
+    parser.add_argument("command", nargs="?", type=str, help='Command to send. If command is multiple words, enclose in " ".')
+    parser.add_argument("--port", nargs="?", type=int, default=DEFAULT_SERVER_PORT, help=f"The port to connect to the server. Default is {DEFAULT_SERVER_PORT}.")
+    parser.add_argument(
+        "--config",
+        help="JSON config file with multiple hosts"
+    )
+    return parser.parse_args()
+
+def load_single_host(hostname: str, port: int, command: str) -> list:
+    return [{
+        "hostname": hostname,
+        "port": port,
+        "command": command
+    }]
+
 async def main(args=None):
-    if args is None:
-        args = sys.argv[1:]
+    if len(sys.argv) < 2:
+        logger.info('Type "SendCmdClient.exe -h" or "SendCmdClient.exe --help" for usage.')
+        return
+    args = parse_args()
 
-        if len(sys.argv) < 1:
-            logger.info('Type "SendCmdClient.exe -h" or "SendCmdClient.exe --help" for usage.')
-            return
+    try:
+        if args.config:
+            # CONFIG MODE: ignore positionals
+            config_data = read_config(args.config)
+        else:
+            # SINGLE HOST MODE : positionals required
+            if not all([args.hostname, args.port, args.command]):
+                sys.exit(
+                    "Error: hostname, and command are required unless --config is specified"
+                )
+            config_data = load_single_host(args.hostname, args.port, args.command)
 
-    parser = argparse.ArgumentParser(description="Client for sending commands to the server.")
-    parser.add_argument("--config", type=str, default="config.json", help="Name of JSON file.")
-    args = parser.parse_args(args)
-
-    config           = args.config
-    if config:
-        try:
-            config_data = read_config(config)
-
-        except Exception as e:
-            print(f"Error: {e}")
-            sys.exit(1)
-
+    except Exception as e:
+        sys.exit(f"Error: {e}")
 
     tasks = [asyncio.create_task(tcp_echo_client(host["hostname"], host["port"], host["command"])) for host in config_data]
     results = await asyncio.gather(*tasks, return_exceptions=True)
     print(f"Task Results: {results}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
