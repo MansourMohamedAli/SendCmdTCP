1c9c1b1e6a39d37f4e9636c7b8f8698d41193edb
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2025-12-28 12:49:55 -0500

    all previous server code integrated. switching directories, setting environment, and splitting at ;

--- SendCmdServer.py
+++ SendCmdServer.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import sys
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 from logger import logger
 
@@ -13,19 +14,45 @@ DEFAULT_SERVER_PORT = 52000
 DEFAULT_MAX_ATTEMPTS = 1  # Maximum number of connection attempts
 
 
-def execute_command_sequential(commands):
-    for cmd in commands:
-        execute_command(cmd)
+def execute_command_sequential(commands_json):
+    for command in commands_json:
+        cmds = command.split(";") if ";" in command else [command]
+        for cmd in cmds:
+            if cmd:
+                if cmd.lower() == "exit":
+                    logger.info("Exiting...")
+                    os._exit(0)
+                # Check if the command is a "cd" command
+                if cmd.startswith("cd "):
+                    try:
+                        new_dir = cmd[3:].strip()
+                        os.chdir(new_dir)
+                        cwd = Path.cwd()  # Update the current working directory
+                        logger.info(cmd)
+                    except FileNotFoundError as e:
+                        logger.error(f"Error: {e}")
+                elif len(cmd) > 1 and cmd[1] == ":":  # Changing Drive
+                    new_dir = cmd[:2].strip()
+                    os.chdir(new_dir)
+                    cwd = Path.cwd()  # Update the current working directory
+                    logger.info(cmd)
+                elif cmd.startswith("set "):
+                    try:
+                        set_cmd = cmd[4:].strip().split("=")
+                        os.environ[set_cmd[0]] = set_cmd[1]
+                        logger.info(cmd)
+                    except FileNotFoundError as e:
+                        logger.error(f"Error: {e}")
+                else:
+                    # Execute the command and get the output
+                    logger.info(f"Executing {cmd}")
+                    execute_command(cmd, cwd)
 
 
-def execute_command(cmd) -> tuple[int, str, str]:
+def execute_command(cmd, cwd) -> tuple[int, str, str]:
     print(f"Executing {cmd}")
-    if cmd.startswith("set "):
-        set_cmd = cmd[4:].strip().split("=")
-        os.environ[set_cmd[0]] = set_cmd[1]
-
     # result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    result = subprocess.run(cmd, shell=True, text=True)
+    result = subprocess.run(cmd, shell=True, text=True, cwd=cwd)
 
     print(f"[{cmd!r} exited with {result.returncode}]")
 
@@ -42,16 +69,17 @@ def execute_command(cmd) -> tuple[int, str, str]:
     #     print(f"[stderr]\n{result.stderr}")
     # return result.returncode, result.stdout, result.stderr
     #################################################################################
+
     return result.returncode
 
 
 async def handle_client(reader, writer):
     data = await reader.read(4096)
-    commands = json.loads(data.decode("utf-8"))
+    commands_list = json.loads(data.decode("utf-8"))
     addr = writer.get_extra_info("peername")
     executor = ThreadPoolExecutor(max_workers=1)
     print(f"Command from {addr}")
-    executor.submit(execute_command_sequential, commands)
+    executor.submit(execute_command_sequential, commands_list)
 
 
 async def main(args=None) -> None:
